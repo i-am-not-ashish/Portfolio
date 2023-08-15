905c82e58fb5213288d8fe7b9bdeb63d8272befc
i-am-not-ashish <darklegend9912@gmail.com>
2023-08-15 17:38:20 +0530

    changeed navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="/navbarSupportedContent"
+            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
